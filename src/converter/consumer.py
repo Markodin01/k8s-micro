f17589eaa2a80f4c6340bfe73d0d2d6fbe9846fb
Marek Gajewski <marek.gajewski2001@gmail.com>
2023-12-30 12:13:46 +0100

    consumer deploy

--- src/converter/consumer.py
+++ src/converter/consumer.py
@@ -1,40 +1,47 @@
-import pika, sys, os, time
+import pika
+import sys
+import os
 from pymongo import MongoClient
 import gridfs
-from converter import to_mp3
+from convert import to_mp3
+import logging
+
+logging.basicConfig(level=logging.INFO)  # Set the desired logging level
 
 def main():
-    client = MongoClient('host.minikube.internal', 27017)
-    db_videos = client.videos
-    db_mp3s = client.mp3s
+    try:
+        client = MongoClient('host.minikube.internal', 27017)
+        db_videos = client.videos
+        db_mp3s = client.mp3s
 
-    fs_videos = gridfs.GridFS(db_mp3s)
-    fs_mp3s = gridfs.GridFS(db_mp3s)
+        fs_videos = gridfs.GridFS(db_mp3s)
+        fs_mp3s = gridfs.GridFS(db_mp3s)
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+        channel = connection.channel()
 
-    channel = connection.channel()
+        def callback(ch, method, properties, body):
+            try:
+                err = to_mp3(body, fs_videos, fs_mp3s, ch)
+                if err:
+                    ch.basic_nack(delivery_tag=method.delivery_tag)
+                else:
+                    ch.basic_ack(delivery_tag=method.delivery_tag)
+            except Exception as e:
+                logging.error(f"Error processing message: {e}")
+                ch.basic_nack(delivery_tag=method.delivery_tag)
 
-    def callback(ch, method, properties, body):
-        err = to_mp3(body, fs_videos, fs_mp3s, ch)
-        if err:
-            ch.basic_nack(delivery_tag=method.delivery_tag)
-        else:
-            ch.basic_ack(delivery_tag=method.delivery_tag)
+        channel.basic_consume(queue=os.environ.get("VIDEO_QUEUE"), on_message_callback=callback)
 
+        logging.info("Waiting for messages...")
 
-    channel.basic_consumer(queue=os.environ.get("VIDEO_QUEUE"), on_message_callback=callback, auto_ack=True)
+        channel.start_consuming()
 
-    print("Waiting for messages...")
-
-    channel.start_consuming()
+    except KeyboardInterrupt:
+        logging.info('Interrupted')
+    finally:
+        connection.close()  # Close RabbitMQ connection
+        client.close()      # Close MongoDB client
 
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+    main()
--- src/converter/convert/to_mp3.py
+++ src/converter/convert/to_mp3.py
@@ -1,40 +1,41 @@
-import pika, json, tempfile, os
+import pika
+import json
+import tempfile
+import os
 from bson.objectid import ObjectId
 import moviepy.editor as mp
 
 def start(message, fs_videos, fs_mp3s, channel):
     message = json.loads(message)
 
-    tf = tempfile.NamedTemporaryFile()
+    # Use a single with statement for file operations
+    with tempfile.NamedTemporaryFile(delete=False) as tf:
+        out = fs_videos.get(ObjectId(message["video_fid"])).read()
+        tf.write(out)
 
-    out = fs_videos.get(ObjectId(message["video_fid"])).read()
-
-    tf.write(out.read())
-
-    audio = mp.VideoFileClip(tf.name).audio
-
-    tf.close()
-
+    # Use moviepy to extract audio
     tf_path = tempfile.gettempdir() + f"/{message['video_fid']}.mp3"
+    audio = mp.VideoFileClip(tf.name).audio
     audio.write_audiofile(tf_path)
 
+    # Read the audio file and upload it to fs_mp3s
     with open(tf_path, "rb") as f:
         data = f.read()
         fid = fs_mp3s.put(data)
-        f.close()
         os.remove(tf_path)
 
-        message["mp3_fid"] = str(fid)
+    message["mp3_fid"] = str(fid)
 
-        try:
-            channel.basic_publish(
-                exchange='',
-                routing_key=os.environ.get("MP3_QUEUE"),
-                body=json.dumps(message),
-                properties=pika.BasicProperties(
-                    delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE,
-                )
+    try:
+        channel.basic_publish(
+            exchange='',
+            routing_key=os.environ.get("MP3_QUEUE"),
+            body=json.dumps(message),
+            properties=pika.BasicProperties(
+                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE,  # or use 2 directly
             )
-        except Exception as err:
-            fs_mp3s.delete(fid)
-            return "failed to publish message to mp3 queue"
+        )
+    except Exception as err:
+        fs_mp3s.delete(fid)
+        print(f"Failed to publish message to mp3 queue. Error: {err}")
+        return "failed to publish message to mp3 queue"
